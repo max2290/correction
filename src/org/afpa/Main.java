1914cd644afaa4c2b35c3e4124ed8d9812d3b2f1
Germain SIPIERE <43010217+germainsip@users.noreply.github.com>
2021-04-22 13:50:40 +0200

    c'est en core mieux

--- src/org/afpa/Main.java
+++ src/org/afpa/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 
 
-        afficheTriangle(2);
+        afficheTriangle();
 
         nbVoyelles();
 
--- src/org/afpa/tools/Exercices.java
+++ src/org/afpa/tools/Exercices.java
@@ -3,9 +3,14 @@ package org.afpa.tools;
 import java.util.Scanner;
 
 public class Exercices {
-    public static void afficheTriangle(int nbLignes) {
-
-
+    public static void afficheTriangle() {
+        Scanner sc = new Scanner(System.in);
+        int nbLignes;
+        System.out.println("===========================================");
+        System.out.println("|      Et si on dessinait un triangle     |");
+        System.out.println("===========================================");
+        System.out.println("Entrez un nombre de lignes: ");
+        nbLignes = sc.nextInt();
         //affichage du triangle
         for (int i = 1; i < nbLignes + 1; i++) {
             System.out.println(" ".repeat(nbLignes - i) + "*".repeat(2 * i - 1));
