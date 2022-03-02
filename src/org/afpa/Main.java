f723b00b81cf8c70c06d64364946dd8a6c7e8827
Germain SIPIERE <gsipiere@gmail.com>
2022-03-02 11:51:01 +0100

    sans fonctions

--- src/org/afpa/Main.java
+++ src/org/afpa/Main.java
@@ -1,21 +1,46 @@
 package org.afpa;
 
-import org.afpa.tools.Exercices;
+
 
 import java.util.Scanner;
 
-import static org.afpa.tools.Exercices.*;
+
 
 
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int nbLignes;
+        String mot;
+        int nbVoyelles = 0;
+        String VOYELLES = "aeiouy";
 
 
-        afficheTriangle();
+        System.out.println("===========================================");
+        System.out.println("|      Et si on dessinait un triangle     |");
+        System.out.println("===========================================");
+        System.out.println("Entrez un nombre de lignes: ");
+        nbLignes = sc.nextInt();
+        //affichage du triangle
+        for (int i = 1; i < nbLignes + 1; i++) {
+            System.out.println(" ".repeat(nbLignes - i) + "*".repeat(2 * i - 1));
+        }
 
-        nbVoyelles();
+
+
+
+        System.out.println("===========================================");
+        System.out.println("| Maintenant je vais compter les voyelles |");
+        System.out.println("===========================================");
+        System.out.println("Entrez un mot: ");
+        mot = sc.next().toLowerCase();
+        for (char a : mot.toCharArray()) {
+            nbVoyelles = (VOYELLES.indexOf(a) >= 0) ? (nbVoyelles + 1) : nbVoyelles;
+        }
+        System.out.printf("Il y a %d voyelle(s) dans %s.\n", nbVoyelles, mot);
+
 
 
         System.out.println("===========================================");
